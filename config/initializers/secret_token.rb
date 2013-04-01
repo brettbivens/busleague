7603c26b591fe7d554a7a2ff6e03fed7e3f8580d
OpenShift guest <brett.bivens@gmail.com>
2013-04-01 04:36:19 -0400

    created rails app and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Busleague::Application.config.secret_token = '7f0cf1246b65b60423cb836e8d12fde035fb57f0b722afad0591a98054bfccf9649cad12c489393a1f50608b9433dd1d8974835975aa3d2df5a52c3cd0bb0876'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Busleague::Application.config.session_store :cookie_store, key: '_busleague_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Busleague::Application.config.session_store :active_record_store
